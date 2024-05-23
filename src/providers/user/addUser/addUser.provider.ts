cb157250efac619d049ce7274498a9c1be0f0e60
Sebas <sebastian.ituarte@gmail.com>
2024-05-22 22:58:55 -0400

    feat: added pinia, added http infrastructure
    feat: added pinia, added http infrastructure
    
    clean architecture for calling providers
    
    added pinia to centralize the api plugin calls into the providers with pinia.

--- src/providers/user/addUser/addUser.provider.ts
+++ src/providers/user/addUser/addUser.provider.ts
@@ -5,27 +5,28 @@ import { ResponseDomain } from '~/domain/response.domain';
 import type { UserDomain } from '~/domain/user.domain';
 // interface import
 import type { IAddUserProvider } from './addUser.interface';
-// provider main class import
+// provider import
 import { Provider } from '~/providers/provider';
 
 export class AddUserProvider extends Provider implements IAddUserProvider {
+  // method to create a new user
   public async create(user: UserDomain): Promise<ResponseDomain> {
     try {
       // call the api
-      const response: AxiosResponse<ResponseDomain> = await this.http.post(
-        `${this.environmentConfig.CLEAN_ARCHITECTURE_API}/user`,
-        JSON.stringify(user),
-        {
-          headers: {
-            'Content-Type': 'application/json',
+      const response: AxiosResponse<ResponseDomain> =
+        await this.http.client.post(
+          `${this.environmentConfig.CLEAN_ARCHITECTURE_API}/user`,
+          JSON.stringify(user),
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
           },
-        },
-      );
+        );
       // return api response
       return new ResponseDomain(response.data);
-    } catch (error: any) {
-      console.error(`${AddUserProvider.name} error ${error}`);
-      return new ResponseDomain(error.data);
+    } catch (error: unknown) {
+      return this.http.handleHttpError(error);
     }
   }
 }
