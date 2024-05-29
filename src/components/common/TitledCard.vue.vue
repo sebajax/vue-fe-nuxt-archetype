18b280e44ab0f59a5660c75ef65756a6416ac8b3
RaulSilvaA <rausilva@ug.uchile.cl>
2024-05-29 18:36:13 -0400

    feat: add Card, Text, TextData components and patients view example

---
+++ src/components/common/TitledCard.vue.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  title: string;
+}>();
+</script>
+
+<template>
+  <div>
+    <v-card class="rounded-lg" elevation="0">
+      <v-card-title class="title-left text-title">
+        <span class="text-lg">{{ props.title }}</span>
+      </v-card-title>
+      <hr class="mx-4" />
+      <v-card-text>
+        <slot></slot>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
