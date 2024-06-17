4a6f687fe5ca6b72df813b37141ba9d159824e2b
RaulSilvaA <rausilva@ug.uchile.cl>
2024-06-17 11:09:56 -0400

    fix: change app base

--- src/app.vue
+++ src/app.vue
@@ -1,41 +1,10 @@
-<script setup lang="ts">
-// [Imports]
-import { useTheme } from 'vuetify';
-// - types
-import type { Theme } from './interfaces/theme.type';
-
-// VUE
-
-// [Modularity - Actual page]
-const actualPage = ref<string>('');
-
-// [Modularity - Set application theme]
-const theme = useTheme();
-const defaultTheme = ref<Theme>('light');
-const themeLanguaje = ref<string>(
-  { light: 'claro', dark: 'oscuro' }[defaultTheme.value],
-);
-// -set default theme based on the swtich value
-watch(defaultTheme, (newTheme: Theme) => {
-  themeLanguaje.value = { light: 'claro', dark: 'oscuro' }[newTheme];
-  theme.global.name.value = newTheme;
-});
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <NuxtLayout>
-    <v-app class="d-flex">
-      <v-layout column>
-        <BaseSideBar v-model="defaultTheme" :sp-theme="themeLanguaje" />
-        <BaseHeader v-model="actualPage" />
-        <v-main :style="{ height: '100vh' }">
-          <NuxtPage
-            class="pt-5 pr-8 overflow-y-scroll"
-            @set-page-title="(value: string) => (actualPage = value)"
-          />
-        </v-main>
-      </v-layout>
-    </v-app>
+    <v-container>
+      <NuxtPage />
+    </v-container>
   </NuxtLayout>
 </template>
 
