330bf03e9debca818724b599ee797de472793b82
RaulSilvaA <rausilva@ug.uchile.cl>
2024-06-18 12:31:47 -0400

    fix: add clientOnly tag to the BaseModal component to avoid hydratation missmatch

--- src/components/common/BaseModal.vue
+++ src/components/common/BaseModal.vue
@@ -13,31 +13,33 @@ const props = withDefaults(defineProps<IProps>(), {
 </script>
 
 <template>
-  <v-dialog :max-width="props.maxWidth">
-    <template #activator="{ props: activatorProps }">
-      <BaseButton v-bind="activatorProps" label="Formulario" />
-    </template>
-    <template #default="{ isActive }">
-      <!-- If the component has title -->
-      <v-card v-if="props.title" :title="props.title">
-        <div class="px-6">
-          <slot />
-        </div>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn text="Cerrar" @click="isActive.value = false" />
-        </v-card-actions>
-      </v-card>
-      <!-- Card without title -->
-      <v-card v-else>
-        <div class="px-6">
-          <slot />
-        </div>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn text="Cerrar" @click="isActive.value = false" />
-        </v-card-actions>
-      </v-card>
-    </template>
-  </v-dialog>
+  <ClientOnly>
+    <v-dialog :max-width="props.maxWidth">
+      <template ssr #activator="{ props: activatorProps }">
+        <BaseButton v-bind="activatorProps" label="Formulario" />
+      </template>
+      <template ssr #default="{ isActive }">
+        <!-- If the component has title -->
+        <v-card v-if="props.title" :title="props.title">
+          <div class="px-6">
+            <slot />
+          </div>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn text="Cerrar" @click="isActive.value = false" />
+          </v-card-actions>
+        </v-card>
+        <!-- Card without title -->
+        <v-card v-else>
+          <div class="px-6">
+            <slot />
+          </div>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn text="Cerrar" @click="isActive.value = false" />
+          </v-card-actions>
+        </v-card>
+      </template>
+    </v-dialog>
+  </ClientOnly>
 </template>
