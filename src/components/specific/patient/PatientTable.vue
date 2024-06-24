5169dbe3f9e9c5c9c5ea8772b6b45a9064284f1b
RaulSilvaA <rausilva@ug.uchile.cl>
2024-06-24 15:18:54 -0400

    feat: add details to redirect to a patient details in the table

--- src/components/specific/patient/PatientTable.vue
+++ src/components/specific/patient/PatientTable.vue
@@ -15,11 +15,28 @@ async function getPatients(event: unknown) {
   // TODO: change to real API
   console.log(options.value);
 }
+
+// [Modularity - Table structure]
+// - [Reactivity State]
+const headers = ref([
+  { title: 'RUT', value: 'rut', sortable: true },
+  { title: 'Nombre', value: 'nombre', sortable: true },
+  { title: 'Categoría', value: 'categoría', sortable: true },
+  { title: 'Urgencia', value: 'urgencia', sortable: true },
+  { title: 'Detalles', value: 'id', sortable: false },
+]);
 </script>
 
 <template>
   <div class="rounded-lg overflow-hidden">
-    <v-data-table :items="patients" @update:options="getPatients($event)" />
+    <v-data-table :items="patients" :headers="headers" items-per-page="10">
+      <template #item.id="{ item }">
+        <!-- Redirection to details -->
+        <NuxtLink :to="`/patient-details/${item.id}`">
+          <v-icon color="primary">mdi-file-tree</v-icon>
+        </NuxtLink>
+      </template>
+    </v-data-table>
   </div>
 </template>
 
